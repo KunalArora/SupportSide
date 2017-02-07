111a92eaee319552757663368b6c53363d68ff53
aroraku <kunal.arora@brother.co.jp>
2017-02-07 15:40:45 +0900

    Initialize

--- app/controllers/concerns/device_setting_action.rb
+++ app/controllers/concerns/device_setting_action.rb
@@ -4,7 +4,7 @@ module DeviceSettingAction
   extend ActiveSupport::Concern
 
   def settings device_id
-    req = Net::HTTP::Post.new('/svc_api/devices/get', {'User-Agent' => 'xmpp_test'})
+    req = Net::HTTP::Post.new('/svc_api/devices/get', 'User-Agent' => 'xmpp_test')
     req.set_form_data(
       'service_id' => Rails.application.secrets.boc_service_id,
       'device_id' => device_id,
@@ -17,44 +17,51 @@ module DeviceSettingAction
       'object_id[6]' => '1.3.6.1.4.1.2435.2.4.4.99.1.1.1.2.1',
       'object_id[7]' => '1.3.6.1.4.1.2435.2.4.4.99.1.1.1.3.1'
     )
-    JSON.parse(BOC::API.new(Rails.application.secrets.boc_api_endpoint).connect(req))
+    send_req(req)
   end
 
   def delete device_id
-    req = Net::HTTP::Post.new('/bol_api/devices/delete', {'User-Agent' => 'xmpp_test'})
+    req = Net::HTTP::Post.new('/bol_api/devices/delete', 'User-Agent' => 'xmpp_test')
     req.set_form_data('owner_id' => '2', 'device_id' => device_id)
-    JSON.parse(BOC::API.new(Rails.application.secrets.boc_api_endpoint).connect(req))
+    send_req(req)
   end
 
   def reboot device_id
-    req = Net::HTTP::Post.new('/svc_api/devices/set', {'User-Agent' => 'xmpp_test'})
+    req = Net::HTTP::Post.new('/svc_api/devices/set', 'User-Agent' => 'xmpp_test')
     req.set_form_data(
       'service_id' => Rails.application.secrets.boc_service_id,
       'device_id' => device_id,
       'device_info[0][object_id]' => '1.3.6.1.4.1.1240.2.3.4.2.3.0',
       'device_info[0][value]' => '21930'
     )
-    JSON.parse(BOC::API.new(Rails.application.secrets.boc_api_endpoint).connect(req))
+    send_req(req)
   end
 
   def enable device_id
-    req = Net::HTTP::Post.new('/svc_api/devices/set', {'User-Agent' => 'xmpp_test'})
+    req = Net::HTTP::Post.new('/svc_api/devices/set', 'User-Agent' => 'xmpp_test')
     req.set_form_data(
       'service_id' => Rails.application.secrets.boc_service_id,
       'device_id' => device_id,
       'device_info[0][object_id]' => '1.3.6.1.4.1.2435.2.4.3.2435.5.36.14.0',
       'device_info[0][value]' => '1'
     )
-    JSON.parse(BOC::API.new(Rails.application.secrets.boc_api_endpoint).connect(req))
+    send_req(req)
   end
 
   def search_value object_id, device_id
-    req = Net::HTTP::Post.new('/svc_api/devices/get', {'User-Agent' => 'xmpp_test'})
+    req = Net::HTTP::Post.new('/svc_api/devices/get', 'User-Agent' => 'xmpp_test')
     req.set_form_data(
       'service_id' => Rails.application.secrets.boc_service_id,
       'device_id' => device_id,
       'object_id[0]' => object_id
     )
-    JSON.parse(BOC::API.new(Rails.application.secrets.boc_api_endpoint).connect(req))
+    send_req(req)
+  end
+
+  private
+
+  def send_req request
+    request.basic_auth(*Rails.application.secrets.boc_api_credentials.split(/:/))
+    JSON.parse(BOC::API.new(Rails.application.secrets.boc_api_endpoint).connect(request))
   end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -1,23 +1,47 @@
-#!/usr/bin/env puma
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum, this matches the default thread size of Active Record.
+#
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
 
-threads 1, 6
+# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
+#
+port        ENV.fetch("PORT") { 3000 }
 
-app_path = '/opt/boc/apps/boc_support_site/current'
-shared_dir = '/opt/boc/apps/boc_support_site/shared'
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-rails_env = 'development'
-environment rails_env
-daemonize true
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked webserver processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-bind "tcp://0.0.0.0:8081"
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory. If you use this option
+# you need to make sure to reconnect any threads in the `on_worker_boot`
+# block.
+#
+# preload_app!
 
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+# The code in the `on_worker_boot` will be called if you are using
+# clustered mode by specifying a number of `workers`. After each worker
+# process is booted this block will be run, if you are using `preload_app!`
+# option you will want to use this block to reconnect to any threads
+# or connections that may have been created at application boot, Ruby
+# cannot share connections between processes.
+#
+# on_worker_boot do
+#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+# end
 
-pidfile "#{shared_dir}/tmp/pids/puma.pid"
-state_path "#{shared_dir}/tmp/pids/puma.state"
-
-on_worker_boot do
-	require 'active_record'
-	ActiveRecord::Base.connection.disconnect!
-end
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
