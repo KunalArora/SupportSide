134b4bf8f44bc7c3b5e680e5f49ab9003c7110ca
aroraku <kunal.arora@brother.co.jp>
2017-02-10 17:02:22 +0900

    Initialize

--- app/helpers/admin/staffs_helper.rb
+++ app/helpers/admin/staffs_helper.rb
@@ -1,2 +1,12 @@
 module Admin::StaffsHelper
+  def admin_type admin
+    case admin
+    when 'sadmin'
+      'Super Administrator'
+    when 'admin'
+      'Administrator'
+    when 'mstaff'
+      'Managing Staff'
+    end
+  end
 end
--- app/helpers/staffs_helper.rb
+++ app/helpers/staffs_helper.rb
@@ -1,11 +1,2 @@
 module StaffsHelper
-  def admin_type admin
-    if admin == 'sadmin'
-      'Super Administrator'
-    elsif admin == 'admin'
-      'Administrator'
-    else
-      'Managing Staff'
-    end
-  end
 end
