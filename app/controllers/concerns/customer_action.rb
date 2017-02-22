7da3b3dc704ae3e71b465051af7be56b1762aaa5
aroraku <kunal.arora@brother.co.jp>
2017-02-22 09:31:25 +0900

    Modification for PIN Generation

--- app/controllers/concerns/customer_action.rb
+++ app/controllers/concerns/customer_action.rb
@@ -1,4 +1,5 @@
 require 'api_call'
+require 'staff.rb'
 
 module CustomerAction
   extend ActiveSupport::Concern
@@ -6,7 +7,7 @@ module CustomerAction
   def generate cust_uid
     req = Net::HTTP::Post.new('/bol_api/devices/pin/create_multidevice')
     req.set_form_data(
-      'owner_id' => 2, 'uid' => cust_uid, 'device_count' => 1,
+      'owner_id' => logged_in_staff.company_id, 'uid' => cust_uid, 'device_count' => 1,
       'expires' => (Time.now + 6.months).iso8601, 'can_delete' => 1
     )
     JSON.parse(BOC::API.new(Rails.application.secrets.boc_api_endpoint).connect(req))
@@ -15,8 +16,14 @@ module CustomerAction
   def outstanding cust_uid
     req = Net::HTTP::Post.new('/bol_api/accounts/pin/outstanding')
     req.set_form_data(
-      'owner_id' => 2, 'uid' => cust_uid
+      'owner_id' => logged_in_staff.company_id, 'uid' => cust_uid
     )
     JSON.parse(BOC::API.new(Rails.application.secrets.boc_api_endpoint).connect(req))
+  end
+
+  private
+
+  def logged_in_staff
+    Staff.find_by(email: session[:email])
   end
 end
