780f9abfee6070f78d25abafd165935b80bd77ae
aroraku <kunal.arora@brother.co.jp>
2017-03-06 09:48:19 +0900

    Resolved Expiration problem

--- app/controllers/concerns/customer_action.rb
+++ app/controllers/concerns/customer_action.rb
@@ -8,7 +8,7 @@ module CustomerAction
     req = Net::HTTP::Post.new('/bol_api/devices/pin/create_multidevice')
     req.set_form_data(
       'owner_id' => logged_in_staff.company_id, 'uid' => cust_uid, 'device_count' => 1,
-      'expires' => (Time.now + 6.months).iso8601, 'can_delete' => 1
+      'expires' => (Time.now + 6.months - 1.day).iso8601, 'can_delete' => 1
     )
     JSON.parse(BOC::API.new(Rails.application.secrets.boc_api_endpoint).connect(req))
   end
